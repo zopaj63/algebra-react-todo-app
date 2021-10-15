12f4466894a5af242ed0b0d9009792379be8649c
zopaj63 <zoran.pajnic@gmail.com>
2021-10-15 20:55:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [ todoState, setTodoState ] = useState([]);
+  const [ formState, setFormState ] = useState({ text: "" });
+
+  const handleOnChange = (event) => {
+    event.preventDefault();
+    setFormState((formState) => ({
+      ...formState,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    setTodoState(todoState => [
+      ...todoState,
+      { ...formState, isDone: false }
+    ]);
+    setFormState({ text: "" });
+  }
+
+  const handleOnDelete = (item, index) => (event) => {
+    setTodoState((todoState) => [
+      ...todoState.slice(0, index),
+      ...todoState.slice(index + 1),
+    ]);
+  }
+
+  const handleOnToggle = (item, index) => (event) => {
+    setTodoState((todoState) => [
+      ...todoState.slice(0, index),
+      { ...item, isDone: !item.isDone },
+      ...todoState.slice(index + 1),
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {todoState.map((item, index) =>
+          <div key={index}>
+            <button type="button"
+              onClick={handleOnToggle(item, index)}
+            >
+              {item.isDone ? "Mark as todo" : "Mark as done"}
+            </button>
+            {item.text}
+            <button type="button"
+              onClick={handleOnDelete(item, index)}
+            >
+              Delete
+            </button>
+          </div>
+        )}
+      </div>
+      <form onSubmit={handleOnSubmit}>
+        <input name="text" type="text" placeholder="Item text"
+          onChange={handleOnChange}
+          value={formState.text}
+        />
+        <button type="submit">Add Item</button>
+      </form>
     </div>
   );
 }
